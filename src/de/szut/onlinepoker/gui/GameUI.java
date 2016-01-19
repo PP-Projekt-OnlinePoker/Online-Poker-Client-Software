a8d128217a2fbe971cc450b0717f636723550b45
Kevin Neumeyer <kevin@kpro-media.de>
2016-01-19 10:19:45 +0100

    Grundgerüst für Rendering

--- src/de/szut/onlinepoker/gui/GameUI.java
+++ src/de/szut/onlinepoker/gui/GameUI.java
@@ -1,6 +1,6 @@
-package de.szut.onlinepoker;
+package de.szut.onlinepoker.gui;
 
-import org.lwjgl.Sys;
+import de.szut.onlinepoker.game.Player;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.newdawn.slick.opengl.Texture;
@@ -17,29 +17,32 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11.glGenTextures;
 import static org.lwjgl.system.MemoryUtil.*;
 
-public class GameUI {
+public class GameUI extends Thread implements GUI {
 
     // We need to strongly reference callback instances.
     private GLFWErrorCallback errorCallback;
     private GLFWKeyCallback keyCallback;
 
-    private Texture cardTexture;
+    private Texture[] cardTextures;
+
 
     // The window handle
     private long window;
 
+    @Override
     public void run() {
-        System.out.println("Running LWJGL " + Sys.getVersion());
-
         try {
+            //Spiel initialisieren
             init();
+
+            //Gameloop
             loop();
 
-            // Release window and window callbacks
+            //Fenster wieder zerstören
             glfwDestroyWindow(window);
             keyCallback.release();
-        } catch(IOException ex) {
-
+        } catch (IOException ex) {
+            System.out.println("Failed while loading game textures");
         } finally {
             // Terminate GLFW and release the GLFWerrorfun
             glfwTerminate();
@@ -47,15 +50,23 @@ public class GameUI {
         }
     }
 
-    private void init() throws IOException{
-        cardTexture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/meineTextur.png"));
+    private Texture[] loadCardTextures() {
+        //Zwei verschachtelte For-loops für alle Karten
+        //Laden mit: TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/meineTextur.png"));
+
+        return null;
+    }
+
+    private void init() throws IOException {
+        //Texturen für die Karten laden
+        this.loadCardTextures();
 
         // Setup an error callback. The default implementation
         // will print the error message in System.err.
         glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
 
         // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if ( glfwInit() != GL11.GL_TRUE )
+        if (glfwInit() != GL11.GL_TRUE)
             throw new IllegalStateException("Unable to initialize GLFW");
 
         // Configure our window
@@ -69,9 +80,9 @@ public class GameUI {
         int HEIGHT = (int) screenSize.getHeight() - 70;
 
         // Create the window
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Texas Hold'em. Loose your money online!", NULL, NULL);
 
-        if ( window == NULL ){
+        if (window == NULL) {
             throw new RuntimeException("Failed to create the GLFW window");
         }
 
@@ -79,7 +90,7 @@ public class GameUI {
         glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
             @Override
             public void invoke(long window, int key, int scancode, int action, int mods) {
-                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
                     glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
             }
         });
@@ -104,7 +115,7 @@ public class GameUI {
         glfwShowWindow(window);
     }
 
-    private void loop() {
+    private synchronized void loop() {
         // This line is critical for LWJGL's interoperation with GLFW's
         // OpenGL context, or any context that is managed externally.
         // LWJGL detects the context that is current in the current thread,
@@ -117,14 +128,77 @@ public class GameUI {
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
-        while ( glfwWindowShouldClose(window) == GL_FALSE ) {
+        while (glfwWindowShouldClose(window) == GL_FALSE) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
             glfwSwapBuffers(window); // swap the color buffers
-            
+
+            //Rendern
+            render();
 
             // Poll for window events. The key callback above will only be
             // invoked during this call.
             glfwPollEvents();
         }
+    }
+
+    private void render(){
+        renderTable();
+        renderPlayers();
+    }
+
+    private void renderTable(){
+        renderHoleCards();
+    }
+
+    private void renderHoleCards(){
+
+    }
+
+    private void renderPlayers(){
+        renderPlayerCards();
+    }
+
+    private void renderPlayerCards(){
+
+    }
+
+    public synchronized void raise(Player p) {
+        //TODO: Animate Raise
+    }
+
+    public synchronized void fold(Player p) {
+        //TODO: Animate Fold
+    }
+
+    public synchronized void call(Player p) {
+        //TODO: Animate Call
+    }
+
+    public synchronized void bet(Player p) {
+        //TODO: Animate Bet
+    }
+
+    public synchronized void check(Player p) {
+        //TODO: Animate Check
+    }
+
+    public synchronized void allIn(Player p) {
+        //TODO: Animate All In
+    }
+
+    public synchronized void tableBegin() {
+        //TODO: Animate beginning
+    }
+
+    public synchronized void flopRound() {
+        //TODO: Animate Flop
+    }
+
+    public synchronized void turnRound() {
+        //TODO: Animate Turn
+    }
+
+    public synchronized void riverRound() {
+        //TODO: Animate River
     }
 }
