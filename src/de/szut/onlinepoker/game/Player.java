d390a87acffd7ea424e0a44b2104e2b20133f0d1
Kevin Neumeyer <kevin@kpro-media.de>
2016-01-19 09:34:00 +0100

    Playerklasse hinzugefügt

---
+++ src/de/szut/onlinepoker/game/Player.java
@@ -0,0 +1,70 @@
+package de.szut.onlinepoker.game;
+
+public class Player {
+
+    public Integer playerId;
+    public Integer money;
+    public String username;
+    public String vorname;
+    public String nachname;
+    public String eMail;
+    public String password;
+
+    /**
+     * Login constructor
+     * @param username
+     * @param password
+     */
+    public Player(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(Integer playerId) {
+        this.playerId = playerId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
